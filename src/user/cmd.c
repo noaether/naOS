f3ecd5f4e594261b05b3202c9c56433754c436fd
noaether <67566122+noaether@users.noreply.github.com>
2023-08-07 20:37:00 -0400

    * src/user/cmd.c: Add echo, everything else works

--- src/user/cmd.c
+++ src/user/cmd.c
@@ -9,31 +9,37 @@
 
 #include "cmd.h"
 
-void interpret(char string[] , size_t len)
+void interpret(char string[], size_t len)
 {
   const char first_char = string[0];
 
   reverse(string, len);
   log(string, LOG_DEBUG);
 
-  char args[80];
+  char args[len];
 
-  memset(args, 0xA1, 80);
   // The first character is the command
   switch (first_char)
   {
   case 'h': // Help
-    fb_print_after("Commands: h - help, c - clear, l - log, p - play", 69);
+    fb_println("Commands: h - help, c - clear, l - log, e - echo, p - play", 59);
     break;
   case 'c': // Clear
     fb_set_cursor(0);
     fb_clear();
     break;
   case 'l': // e.g. > log hello world
-    strncpy(string, args, len - 4);
+    strncpy(args, string, len - 4);
+    reverse(args, len - 4);
+    log("CMD | Log", LOG_INFO);
     log(args, LOG_INFO);
-    reverse(args, 80);
+    break;
+  case 'e': // e.g. > echo hello world
+    strncpy(args, string, len - 5);
+    reverse(args, len - 5);
+    log("CMD | Echo", LOG_INFO);
     log(args, LOG_INFO);
+    fb_println(args, len - 5);
     break;
   case 'p': // Play
     play_array();
