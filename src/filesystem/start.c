f0af3d7c6139f53448bcb0036ddb8322bf820b70
Noa St-Onge <67566122+noaether@users.noreply.github.com>
2023-09-11 22:49:29 -0400

    switch to debug

--- src/filesystem/start.c
+++ src/filesystem/start.c
@@ -12,27 +12,7 @@ void initializeFileSystem() {
 }
 
 void fs_main() {
-    log("Initializing RAM filesystem...", LOG_INFO);
+    log("FS  | Initializing RAM filesystem...", LOG_DEBUG);
     initializeFileSystem();
-    log("RAM filesystem initialized!", LOG_INFO);
-
-    /*// Example usage
-    createFile("example.txt", 0x06); // Create a file with read and write permissions
-    log("Created file!", LOG_INFO);
-
-    writeFile("example.txt", "Hello, RAM filesystem!", 23);
-    log("Wrote to file!", LOG_INFO);
-
-
-    char buffer[100];
-    int bytesRead = readFile("example.txt", buffer, sizeof(buffer));
-    log("Read file!", LOG_INFO);
-
-    if (bytesRead >= 0) {
-        log("Read file contents !", LOG_INFO);
-        log(buffer, LOG_INFO);
-    }
-
-    deleteFile("example.txt");
-    log("Deleted file!", LOG_INFO);*/
+    log("FS  | RAM filesystem initialized!", LOG_DEBUG);
 }
