e3c63cc71eb71f4ae37038c8c9a5465755eb64b3
Restyled.io <commits@restyled.io>
2023-10-03 00:27:48 +0000

    Restyled by clang-format

--- src/lib/naOS/string.c
+++ src/lib/naOS/string.c
@@ -1,280 +1,243 @@
-#include <stddef.h>
 #include <stdbool.h>
+#include <stddef.h>
 
-#include "string.h"
 #include "../../memory.h"
+#include "string.h"
 
-char *strcat(char *dest, const char *src)
-{
-    char *rdest = dest;
+char *strcat(char *dest, const char *src) {
+  char *rdest = dest;
 
-    while (*dest)
-        dest++;
-    while ((*dest++ = *src++))
-        ;
-    return rdest;
+  while (*dest)
+    dest++;
+  while ((*dest++ = *src++))
+    ;
+  return rdest;
 }
 
-char *strncat(char *dest, char *src, size_t n)
-{
-    while (*dest)
-        dest++;
+char *strncat(char *dest, char *src, size_t n) {
+  while (*dest)
+    dest++;
 
-    while (n-- && *src)
-        *dest = *src, dest++, src++;
+  while (n-- && *src)
+    *dest = *src, dest++, src++;
 
-    *dest = '\0';
-    return dest;
+  *dest = '\0';
+  return dest;
 } // https://stackoverflow.com/q/1996950/17631126
 
-char *strcpy(char *dest, const char *src)
-{
-    char *temp = dest;
-    while ((*dest++ = *src++))
-        ; // or while((*dest++=*src++) != '\0');
-    return temp;
+char *strcpy(char *dest, const char *src) {
+  char *temp = dest;
+  while ((*dest++ = *src++))
+    ; // or while((*dest++=*src++) != '\0');
+  return temp;
 } // https://stackoverflow.com/a/20190538
 
-char *strncpy(char *dest, const char *src, size_t n)
-{
-    char *p;
+char *strncpy(char *dest, const char *src, size_t n) {
+  char *p;
 
-    p = dest;
-    for (; n > 0 && *src != '\0'; --n)
-        *p++ = *src++;
-    for (; n > 0; --n)
-        *p++ = '\0';
-    return dest;
-} // https://clc-wiki.net/wiki/K&R2_solutions:Chapter_5:Exercise_5 https://stackoverflow.com/a/1996991/17631126
+  p = dest;
+  for (; n > 0 && *src != '\0'; --n)
+    *p++ = *src++;
+  for (; n > 0; --n)
+    *p++ = '\0';
+  return dest;
+} // https://clc-wiki.net/wiki/K&R2_solutions:Chapter_5:Exercise_5
+  // https://stackoverflow.com/a/1996991/17631126
 
-size_t strlen(const char *str)
-{
-    if (str == NULL)
-    {
-        return 0;
-    }
-    size_t len = 0;
-    while (str[len] != '\0')
-    {
-        len++;
-    }
-    return len;
+size_t strlen(const char *str) {
+  if (str == NULL) {
+    return 0;
+  }
+  size_t len = 0;
+  while (str[len] != '\0') {
+    len++;
+  }
+  return len;
 }
 
-int strcmp(const char *s1, const char *s2)
-{
-    const unsigned char *p1 = (const unsigned char *)s1;
-    const unsigned char *p2 = (const unsigned char *)s2;
+int strcmp(const char *s1, const char *s2) {
+  const unsigned char *p1 = (const unsigned char *)s1;
+  const unsigned char *p2 = (const unsigned char *)s2;
 
-    while (*p1 && *p1 == *p2)
-        ++p1, ++p2;
+  while (*p1 && *p1 == *p2)
+    ++p1, ++p2;
 
-    return (*p1 > *p2) - (*p2 > *p1);
+  return (*p1 > *p2) - (*p2 > *p1);
 }
 
-int strncmp(const char *s1, const char *s2, size_t n)
-{
-    while (n && *s1 && (*s1 == *s2))
-    {
-        ++s1;
-        ++s2;
-        --n;
-    }
-    if (n == 0)
-    {
-        return 0;
-    }
-    else
-    {
-        return (*(unsigned char *)s1 - *(unsigned char *)s2);
-    }
+int strncmp(const char *s1, const char *s2, size_t n) {
+  while (n && *s1 && (*s1 == *s2)) {
+    ++s1;
+    ++s2;
+    --n;
+  }
+  if (n == 0) {
+    return 0;
+  } else {
+    return (*(unsigned char *)s1 - *(unsigned char *)s2);
+  }
 }
 
 // A utility function to reverse a string
-void reverse(char str[], int length)
-{
-    int start = 0;
-    int end = length - 1;
-    while (start < end)
-    {
-        char temp = str[start];
-        str[start] = str[end];
-        str[end] = temp;
-        end--;
-        start++;
-    }
+void reverse(char str[], int length) {
+  int start = 0;
+  int end = length - 1;
+  while (start < end) {
+    char temp = str[start];
+    str[start] = str[end];
+    str[end] = temp;
+    end--;
+    start++;
+  }
 }
 
-void itoa(int n, char *buf, int buf_size)
-{
-    int i = 0;
-    int is_negative = 0;
+void itoa(int n, char *buf, int buf_size) {
+  int i = 0;
+  int is_negative = 0;
 
-    // Handle negative numbers
-    if (n < 0)
-    {
-        is_negative = 1;
-        n = -n;
-    }
+  // Handle negative numbers
+  if (n < 0) {
+    is_negative = 1;
+    n = -n;
+  }
 
-    // Check if the buffer size is sufficient
-    if (i >= buf_size)
-    {
-        return;
-    }
+  // Check if the buffer size is sufficient
+  if (i >= buf_size) {
+    return;
+  }
 
-    // Convert the integer to a string in reverse order
-    do
-    {
-        buf[i++] = (char)(n % 10 + '0');
-        n /= 10;
-    } while (n > 0);
+  // Convert the integer to a string in reverse order
+  do {
+    buf[i++] = (char)(n % 10 + '0');
+    n /= 10;
+  } while (n > 0);
 
-    // Check if the buffer size is sufficient
-    if (i >= buf_size)
-    {
-        return;
-    }
+  // Check if the buffer size is sufficient
+  if (i >= buf_size) {
+    return;
+  }
 
-    // Add '-' sign for negative numbers
-    if (is_negative)
-    {
-        buf[i++] = '-';
-    }
+  // Add '-' sign for negative numbers
+  if (is_negative) {
+    buf[i++] = '-';
+  }
 
-    // Check if the buffer size is sufficient
-    if (i >= buf_size)
-    {
-        return;
-    }
+  // Check if the buffer size is sufficient
+  if (i >= buf_size) {
+    return;
+  }
 
-    // Reverse the string
-    int j;
-    for (j = 0; j < i / 2; j++)
-    {
-        char temp = buf[j];
-        buf[j] = buf[i - j - 1];
-        buf[i - j - 1] = temp;
-    }
+  // Reverse the string
+  int j;
+  for (j = 0; j < i / 2; j++) {
+    char temp = buf[j];
+    buf[j] = buf[i - j - 1];
+    buf[i - j - 1] = temp;
+  }
 
-    // Null-terminate the string
-    buf[i] = '\0';
+  // Null-terminate the string
+  buf[i] = '\0';
 }
 
 // Implementation of citoa()
-char *citoa(int num, char *str, int base)
-{
-    int i = 0;
-    bool isNegative = false;
+char *citoa(int num, char *str, int base) {
+  int i = 0;
+  bool isNegative = false;
 
-    /* Handle 0 explicitly, otherwise empty string is
-     * printed for 0 */
-    if (num == 0)
-    {
-        str[i++] = '0';
-        str[i] = '\0';
-        return str;
-    }
-
-    // In standard itoa(), negative numbers are handled
-    // only with base 10. Otherwise numbers are
-    // considered unsigned.
-    if (num < 0 && base == 10)
-    {
-        isNegative = true;
-        num = -num;
-    }
-
-    // Process individual digits
-    while (num != 0)
-    {
-        int rem = num % base;
-        str[i++] = (rem > 9) ? (rem - 10) + 'a' : rem + '0';
-        num = num / base;
-    }
-
-    // If number is negative, append '-'
-    if (isNegative)
-        str[i++] = '-';
-
-    str[i] = '\0'; // Append string terminator
-
-    // Reverse the string
-    reverse(str, i);
-
+  /* Handle 0 explicitly, otherwise empty string is
+   * printed for 0 */
+  if (num == 0) {
+    str[i++] = '0';
+    str[i] = '\0';
     return str;
+  }
+
+  // In standard itoa(), negative numbers are handled
+  // only with base 10. Otherwise numbers are
+  // considered unsigned.
+  if (num < 0 && base == 10) {
+    isNegative = true;
+    num = -num;
+  }
+
+  // Process individual digits
+  while (num != 0) {
+    int rem = num % base;
+    str[i++] = (rem > 9) ? (rem - 10) + 'a' : rem + '0';
+    num = num / base;
+  }
+
+  // If number is negative, append '-'
+  if (isNegative)
+    str[i++] = '-';
+
+  str[i] = '\0'; // Append string terminator
+
+  // Reverse the string
+  reverse(str, i);
+
+  return str;
 }
 
-int *create_delim_dict(char *delim)
-{
-    int *d = (int *)malloc(sizeof(int) * DICT_LEN);
-    memset((void *)d, 0, sizeof(int) * DICT_LEN);
+int *create_delim_dict(char *delim) {
+  int *d = (int *)malloc(sizeof(int) * DICT_LEN);
+  memset((void *)d, 0, sizeof(int) * DICT_LEN);
 
-    size_t i;
-    for (i = 0; i < strlen(delim); i++)
-    {
-        d[(unsigned int)delim[i]] = 1;
-    }
-    return d;
+  size_t i;
+  for (i = 0; i < strlen(delim); i++) {
+    d[(unsigned int)delim[i]] = 1;
+  }
+  return d;
 }
 
-char *strtok(char *str, char *delim)
-{
+char *strtok(char *str, char *delim) {
 
-    static char *last, *to_free;
-    int *deli_dict = create_delim_dict(delim);
+  static char *last, *to_free;
+  int *deli_dict = create_delim_dict(delim);
 
-    if (!deli_dict)
-    {
-        return NULL;
-    }
+  if (!deli_dict) {
+    return NULL;
+  }
 
-    if (str)
-    {
-        last = (char *)malloc(strlen(str) + 1);
-        if (!last)
-        {
-            free(deli_dict);
-        }
-        to_free = last;
-        strcpy(last, str);
+  if (str) {
+    last = (char *)malloc(strlen(str) + 1);
+    if (!last) {
+      free(deli_dict);
     }
+    to_free = last;
+    strcpy(last, str);
+  }
 
-    while (deli_dict[(unsigned int)*last] && *last != '\0')
-    {
-        last++;
-    }
-    str = last;
-    if (*last == '\0')
-    {
-        free(deli_dict);
-        free(to_free);
-        return NULL;
-    }
-    while (*last != '\0' && !deli_dict[(unsigned int)*last])
-    {
-        last++;
-    }
-
-    *last = '\0';
+  while (deli_dict[(unsigned int)*last] && *last != '\0') {
     last++;
-
+  }
+  str = last;
+  if (*last == '\0') {
     free(deli_dict);
-    return str;
+    free(to_free);
+    return NULL;
+  }
+  while (*last != '\0' && !deli_dict[(unsigned int)*last]) {
+    last++;
+  }
+
+  *last = '\0';
+  last++;
+
+  free(deli_dict);
+  return str;
 }
 
-char *chopN(char *s, size_t n)
-{
-    char *src = s;
+char *chopN(char *s, size_t n) {
+  char *src = s;
 
-    while (*src && n)
-        --n, ++src;
+  while (*src && n)
+    --n, ++src;
 
-    if (n == 0 && src != s)
-    {
-        for (char *dst = s; (*dst++ = *src++);)
-            ;
-    }
+  if (n == 0 && src != s) {
+    for (char *dst = s; (*dst++ = *src++);)
+      ;
+  }
 
-    return s;
+  return s;
 } // https://stackoverflow.com/a/40538982
--- src/user/cmd.c
+++ src/user/cmd.c
@@ -4,10 +4,10 @@
 
 #include <naOS/string.h>
 
-#include <naOS/mem.h>
 #include <drivers/framebuffer.h>
-#include <utils/log.h>
 #include <drivers/sound.h>
+#include <naOS/mem.h>
+#include <utils/log.h>
 
 #include <filesystem/fileops.h>
 
@@ -18,44 +18,25 @@ int since_enter = 0;
 char *del = " ";
 
 struct note mary_had_a_little_lamb[] = {
-    {OCTAVE_4, NOTE_E, 4},
-    {OCTAVE_4, NOTE_D, 4},
-    {OCTAVE_4, NOTE_C, 4},
-    {OCTAVE_4, NOTE_D, 4},
-    {OCTAVE_4, NOTE_E, 4},
-    {OCTAVE_4, NOTE_E, 4},
-    {OCTAVE_4, NOTE_E, 2},
-    {OCTAVE_4, NOTE_D, 4},
-    {OCTAVE_4, NOTE_D, 4},
-    {OCTAVE_4, NOTE_E, 4},
-    {OCTAVE_4, NOTE_G, 4},
-    {OCTAVE_4, NOTE_G, 4},
-    {OCTAVE_4, NOTE_E, 4},
-    {OCTAVE_4, NOTE_D, 4},
-    {OCTAVE_4, NOTE_C, 4},
-    {OCTAVE_4, NOTE_D, 4},
-    {OCTAVE_4, NOTE_E, 4},
-    {OCTAVE_4, NOTE_E, 4},
-    {OCTAVE_4, NOTE_E, 4},
-    {OCTAVE_4, NOTE_D, 4},
-    {OCTAVE_4, NOTE_D, 4},
-    {OCTAVE_4, NOTE_E, 4},
-    {OCTAVE_4, NOTE_D, 4},
-    {OCTAVE_4, NOTE_D, 4},
-    {OCTAVE_4, NOTE_D, 4},
-    {OCTAVE_4, NOTE_D, 4},
-    {OCTAVE_4, NOTE_E, 4},
-    {OCTAVE_4, NOTE_E, 4},
-    {OCTAVE_4, NOTE_E, 2},
-    {OCTAVE_4, NOTE_D, 4},
-    {OCTAVE_4, NOTE_D, 4},
-    {OCTAVE_4, NOTE_E, 4},
-    {OCTAVE_4, NOTE_G, 4},
-    {OCTAVE_4, NOTE_G, 4},
-    {OCTAVE_4, NOTE_E, 4},
-    {OCTAVE_4, NOTE_D, 4},
-    {OCTAVE_4, NOTE_C, 4},
-    {0, 0, 0} // End of array
+    {OCTAVE_4, NOTE_E, 4}, {OCTAVE_4, NOTE_D, 4},
+    {OCTAVE_4, NOTE_C, 4}, {OCTAVE_4, NOTE_D, 4},
+    {OCTAVE_4, NOTE_E, 4}, {OCTAVE_4, NOTE_E, 4},
+    {OCTAVE_4, NOTE_E, 2}, {OCTAVE_4, NOTE_D, 4},
+    {OCTAVE_4, NOTE_D, 4}, {OCTAVE_4, NOTE_E, 4},
+    {OCTAVE_4, NOTE_G, 4}, {OCTAVE_4, NOTE_G, 4},
+    {OCTAVE_4, NOTE_E, 4}, {OCTAVE_4, NOTE_D, 4},
+    {OCTAVE_4, NOTE_C, 4}, {OCTAVE_4, NOTE_D, 4},
+    {OCTAVE_4, NOTE_E, 4}, {OCTAVE_4, NOTE_E, 4},
+    {OCTAVE_4, NOTE_E, 4}, {OCTAVE_4, NOTE_D, 4},
+    {OCTAVE_4, NOTE_D, 4}, {OCTAVE_4, NOTE_E, 4},
+    {OCTAVE_4, NOTE_D, 4}, {OCTAVE_4, NOTE_D, 4},
+    {OCTAVE_4, NOTE_D, 4}, {OCTAVE_4, NOTE_D, 4},
+    {OCTAVE_4, NOTE_E, 4}, {OCTAVE_4, NOTE_E, 4},
+    {OCTAVE_4, NOTE_E, 2}, {OCTAVE_4, NOTE_D, 4},
+    {OCTAVE_4, NOTE_D, 4}, {OCTAVE_4, NOTE_E, 4},
+    {OCTAVE_4, NOTE_G, 4}, {OCTAVE_4, NOTE_G, 4},
+    {OCTAVE_4, NOTE_E, 4}, {OCTAVE_4, NOTE_D, 4},
+    {OCTAVE_4, NOTE_C, 4}, {0, 0, 0} // End of array
 };
 
 struct note note_to_play[] = {{OCTAVE_4, NOTE_A, 10}};
@@ -98,7 +79,8 @@ struct note note_to_play[] = {{OCTAVE_4, NOTE_A, 10}};
 
 // Function prototypes for command handlers
 /**
- * @brief Handles the writefile command by creating a file with the given name and writing the given string to it.
+ * @brief Handles the writefile command by creating a file with the given name
+ * and writing the given string to it.
  *
  * @param string The string to write to the file.
  * @param len The length of the string.
@@ -107,7 +89,8 @@ struct note note_to_play[] = {{OCTAVE_4, NOTE_A, 10}};
 void handleWriteFile(char *string, size_t len, char *args);
 
 /**
- * @brief Handles the cat command by reading the contents of the given file and printing it to the screen.
+ * @brief Handles the cat command by reading the contents of the given file and
+ * printing it to the screen.
  *
  * @param string The string to write to the file.
  * @param len The length of the string.
@@ -132,200 +115,209 @@ void handleUnknown(char *string, size_t len, char *args);
 void handleChangeDirectory(char *string, size_t len, char *args);
 
 // Define a command table
-struct Command
-{
-    const char *name;
-    const char *desc;
-    const char *usage;
-    void (*handler)(char *, size_t, char *);
-    int numArgs;
-    const char *args[10];
+struct Command {
+  const char *name;
+  const char *desc;
+  const char *usage;
+  void (*handler)(char *, size_t, char *);
+  int numArgs;
+  const char *args[10];
 } __attribute__((packed));
 
 struct Command commands[] = {
-    {"writefile", "Writes a file to the filesystem", "writefile <name> <data>", handleWriteFile, 2, {"name", "data"}},
-    {"cat", "Reads a file from the filesystem", "cat <name>", handleReadFile, 1, {"name"}},
+    {"writefile",
+     "Writes a file to the filesystem",
+     "writefile <name> <data>",
+     handleWriteFile,
+     2,
+     {"name", "data"}},
+    {"cat",
+     "Reads a file from the filesystem",
+     "cat <name>",
+     handleReadFile,
+     1,
+     {"name"}},
     {"help", "Prints this help message", "help", handleHelp, 0, {}},
     {"clear", "Clears the screen", "clear", handleClear, 0, {}},
-    {"log", "Logs a message to the kernel log", "log <data>", handleLog, 1, {"data"}},
-    {"echo", "Prints a message to the screen", "echo <data>", handleEcho, 1, {"data"}},
+    {"log",
+     "Logs a message to the kernel log",
+     "log <data>",
+     handleLog,
+     1,
+     {"data"}},
+    {"echo",
+     "Prints a message to the screen",
+     "echo <data>",
+     handleEcho,
+     1,
+     {"data"}},
     {"play", "Plays a song", "play", handlePlay, 0, {}},
     {"quit", "Quits the kernel", "quit", handleQuit, 0, {}},
-    {"cd", "Changes the current directory", "cd <path>", handleChangeDirectory, 1, {"path"}}
-};
+    {"cd",
+     "Changes the current directory",
+     "cd <path>",
+     handleChangeDirectory,
+     1,
+     {"path"}}};
 
 int numCommands = sizeof(commands) / sizeof(commands[0]);
 
 // Function to interpret a command
-void interpret(char *string, size_t len)
-{
-    char *command = strtok(string, del);
+void interpret(char *string, size_t len) {
+  char *command = strtok(string, del);
 
-    if (command == NULL)
-    {
-        return; // Empty input
+  if (command == NULL) {
+    return; // Empty input
+  }
+
+  // Find the corresponding command handler
+  for (int i = 0; i < numCommands; i++) {
+    if (strcmp(command, commands[i].name) == 0) {
+      commands[i].handler(string, len, command);
+      return;
     }
+  }
 
-    // Find the corresponding command handler
-    for (int i = 0; i < numCommands; i++)
-    {
-        if (strcmp(command, commands[i].name) == 0)
-        {
-            commands[i].handler(string, len, command);
-            return;
-        }
-    }
-
-    handleUnknown(string, len, NULL);
+  handleUnknown(string, len, NULL);
 }
 
-void handleWriteFile(char *string, size_t len, char *args)
-{
-    char endbuffer[256];
-    // Implementation for writefile command
-    args = strtok(NULL, del); // move to arg1
+void handleWriteFile(char *string, size_t len, char *args) {
+  char endbuffer[256];
+  // Implementation for writefile command
+  args = strtok(NULL, del); // move to arg1
 
-    createFile(args, 0x06);
+  createFile(args, 0x06);
 
-    reverse(string, len);
-    strncpy(endbuffer, string, len - strlen("writefile  ") - strlen(args));
-    reverse(endbuffer, strlen(endbuffer));
+  reverse(string, len);
+  strncpy(endbuffer, string, len - strlen("writefile  ") - strlen(args));
+  reverse(endbuffer, strlen(endbuffer));
 
-    writeFile(args, endbuffer, strlen(endbuffer));
+  writeFile(args, endbuffer, strlen(endbuffer));
 
-    fb_println("File written succesfully !", 27);
+  fb_println("File written succesfully !", 27);
 
-    clear(string, sizeof(string));
-    clear(endbuffer, sizeof(endbuffer));
-    clear(args, sizeof(args));
+  clear(string, sizeof(string));
+  clear(endbuffer, sizeof(endbuffer));
+  clear(args, sizeof(args));
 
-    (void)len;
+  (void)len;
 }
 
-void handleReadFile(char *string, size_t len, char *args)
-{
-    char endbuffer[256];
+void handleReadFile(char *string, size_t len, char *args) {
+  char endbuffer[256];
 
-    args = strtok(NULL, del); // arg1
-    readFile(args, endbuffer, sizeof(endbuffer));
+  args = strtok(NULL, del); // arg1
+  readFile(args, endbuffer, sizeof(endbuffer));
 
+  fb_println(endbuffer, strlen(endbuffer));
+
+  clear(string, sizeof(string));
+  clear(endbuffer, sizeof(endbuffer));
+  clear(args, sizeof(args));
+
+  (void)len;
+}
+
+void handleHelp(char *string, size_t len, char *args) {
+  char endbuffer[256];
+
+  for (int i = 0; i < numCommands; i++) {
+    sprintf(endbuffer, "%s - %s : %s", commands[i].name, commands[i].desc,
+            commands[i].usage);
     fb_println(endbuffer, strlen(endbuffer));
+    memset(endbuffer, 0, sizeof(endbuffer));
+  }
+  clear(string, sizeof(string));
+  clear(endbuffer, sizeof(endbuffer));
+  clear(args, sizeof(args));
 
-    clear(string, sizeof(string));
-    clear(endbuffer, sizeof(endbuffer));
-    clear(args, sizeof(args));
-
-    (void)len;
+  (void)len;
 }
 
-void handleHelp(char *string, size_t len, char *args)
-{
-    char endbuffer[256];
+void handleClear(char *string, size_t len, char *args) {
+  char endbuffer[256];
 
-    for (int i = 0; i < numCommands; i++)
-    {
-        sprintf(endbuffer, "%s - %s : %s", commands[i].name, commands[i].desc, commands[i].usage);
-        fb_println(endbuffer, strlen(endbuffer));
-        memset(endbuffer, 0, sizeof(endbuffer));
-    }
-    clear(string, sizeof(string));
-    clear(endbuffer, sizeof(endbuffer));
-    clear(args, sizeof(args));
+  fb_set_cursor(0);
+  fb_clear();
 
-    (void)len;
+  clear(string, sizeof(string));
+  clear(endbuffer, sizeof(endbuffer));
+  clear(args, sizeof(args));
+
+  (void)len;
 }
 
-void handleClear(char *string, size_t len, char *args)
-{
-    char endbuffer[256];
+void handleLog(char *string, size_t len, char *args) {
+  char endbuffer[256];
 
-    fb_set_cursor(0);
-    fb_clear();
+  char *toBeLogged = chopN(string, strlen("log "));
+  sprintf(endbuffer, "CMD | Log: %s", toBeLogged);
+  log(endbuffer, LOG_DEBUG);
 
-    clear(string, sizeof(string));
-    clear(endbuffer, sizeof(endbuffer));
-    clear(args, sizeof(args));
+  clear(string, sizeof(string));
+  clear(endbuffer, sizeof(endbuffer));
+  clear(args, sizeof(args));
 
-    (void)len;
+  (void)len;
 }
 
-void handleLog(char *string, size_t len, char *args)
-{
-    char endbuffer[256];
+void handleEcho(char *string, size_t len, char *args) {
+  char *endbuffer = chopN(string, strlen("echo "));
+  fb_println(endbuffer, strlen(endbuffer));
 
-    char *toBeLogged = chopN(string, strlen("log "));
-    sprintf(endbuffer, "CMD | Log: %s", toBeLogged);
-    log(endbuffer, LOG_DEBUG);
+  clear(string, sizeof(string));
+  clear(endbuffer, sizeof(endbuffer));
+  clear(args, sizeof(args));
 
-    clear(string, sizeof(string));
-    clear(endbuffer, sizeof(endbuffer));
-    clear(args, sizeof(args));
-
-    (void)len;
+  (void)len;
 }
 
-void handleEcho(char *string, size_t len, char *args)
-{
-    char *endbuffer = chopN(string, strlen("echo "));
-    fb_println(endbuffer, strlen(endbuffer));
+void handlePlay(char *string, size_t len, char *args) {
+  char endbuffer[256];
 
-    clear(string, sizeof(string));
-    clear(endbuffer, sizeof(endbuffer));
-    clear(args, sizeof(args));
+  play_array(mary_had_a_little_lamb, 38);
 
-    (void)len;
+  clear(string, sizeof(string));
+  clear(endbuffer, sizeof(endbuffer));
+  clear(args, sizeof(args));
+
+  (void)len;
 }
 
-void handlePlay(char *string, size_t len, char *args)
-{
-    char endbuffer[256];
+void handleQuit(char *string, size_t len, char *args) {
+  char endbuffer[256];
 
-    play_array(mary_had_a_little_lamb, 38);
+  log("CMD | Quit", LOG_DEBUG);
+  asm volatile("hlt");
 
-    clear(string, sizeof(string));
-    clear(endbuffer, sizeof(endbuffer));
-    clear(args, sizeof(args));
+  clear(string, sizeof(string));
+  clear(endbuffer, sizeof(endbuffer));
+  clear(args, sizeof(args));
 
-    (void)len;
+  (void)len;
 }
 
-void handleQuit(char *string, size_t len, char *args)
-{
-    char endbuffer[256];
+void handleUnknown(char *string, size_t len, char *args) {
+  char endbuffer[256];
 
-    log("CMD | Quit", LOG_DEBUG);
-    asm volatile("hlt");
+  // sprintf(endbuffer, "Unknown command: %s", string);
+  fb_println("Unknown command.", 17);
 
-    clear(string, sizeof(string));
-    clear(endbuffer, sizeof(endbuffer));
-    clear(args, sizeof(args));
+  // memset(endbuffer, 0, sizeof(endbuffer));
 
-    (void)len;
+  clear(string, sizeof(string));
+  clear(endbuffer, sizeof(endbuffer));
+  clear(args, sizeof(args));
+
+  (void)len;
 }
 
-void handleUnknown(char *string, size_t len, char *args)
-{
-    char endbuffer[256];
+void handleChangeDirectory(char *string, size_t len, char *args) {
+  char endbuffer[256];
 
-    // sprintf(endbuffer, "Unknown command: %s", string);
-    fb_println("Unknown command.", 17);
+  clear(string, sizeof(string));
+  clear(endbuffer, sizeof(endbuffer));
+  clear(args, sizeof(args));
 
-    // memset(endbuffer, 0, sizeof(endbuffer));
-
-    clear(string, sizeof(string));
-    clear(endbuffer, sizeof(endbuffer));
-    clear(args, sizeof(args));
-
-    (void)len;
-}
-
-void handleChangeDirectory(char *string, size_t len, char *args)
-{
-    char endbuffer[256];
-
-    clear(string, sizeof(string));
-    clear(endbuffer, sizeof(endbuffer));
-    clear(args, sizeof(args));
-
-    (void)len;
+  (void)len;
 }
