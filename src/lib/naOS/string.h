3ba69887201c7d750f4ff0cc04850cfcd5d942fa
Noa St-Onge <67566122+noaether@users.noreply.github.com>
2023-10-08 10:36:54 -0400

    separate things for readability

--- src/lib/naOS/string.h
+++ src/lib/naOS/string.h
@@ -9,7 +9,9 @@ char *strcat(char *dest, const char *src);
 char *strncat(char *dest, char *src, size_t n);
 char *strcpy(char *dest, const char *src);
 char *strncpy(char *s, const char *ct, size_t n);
+
 size_t strlen(const char *str);
+
 int strcmp(const char *s1, const char *s2);
 int strncmp(const char *s1, const char *s2, size_t n);
 
