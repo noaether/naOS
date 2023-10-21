3f8f47a82b11dc09e7602ffdeb207a9acffdb2e2
noae <67566122+noaether@users.noreply.github.com>
2023-10-21 16:42:22 -0400

    Update configure.sh
    Signed-off-by: noae <67566122+noaether@users.noreply.github.com>

--- configure.sh
+++ configure.sh
@@ -1,8 +1,48 @@
 #!/bin/bash
 
+trap ctrl_c INT
+
+function ctrl_c() {
+        sudo umount /tmp/ramdisk
+}
+
+# Calculate half of the available RAM
+total_ram=$(grep MemTotal /proc/meminfo | awk '{print $2}')
+half_ram=$((total_ram / 2))
+
+# By default, don't use the RAM disk
+use_ram=0
+
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --use-ram) use_ram=1; shift ;;
+        *) echo "Unknown parameter: $1"; exit 1 ;;
+    esac
+done
+
+if [ "$use_ram" -eq 1 ]; then
+    sudo mkdir /tmp/ramdisk
+    sudo chmod 777 /tmp/ramdisk
+
+    # Create temporary RAM disk
+    sudo mount -t tmpfs -o size=${half_ram}k tmpfs /tmp/ramdisk
+
+    export RAMDISK_PATH=/tmp/ramdisk
+    mount | tail -n 1
+else
+    export RAMDISK_PATH=""
+fi
+
+# Move to the RAM disk if it's being used
+if [ -n "$RAMDISK_PATH" ]; then
+    cd $RAMDISK_PATH
+fi
+
 export cc_prefix="$HOME/opt/cross"
 export cc_target=i686-elf
 export PATH="$cc_prefix/bin:$PATH"
+
+mkdir -p $cc_prefix
 
 export ncpu=$(($(nproc --all) - 2))  # Number of cores minus 2
 
@@ -11,7 +51,7 @@ if [[ $ncpu -lt 1 ]]; then
     ncpu=1
 fi
 
-# Detect the package manager
+#Detect the package manager
 pkg_manager=""
 
 if command -v apt &>/dev/null; then
@@ -42,12 +82,11 @@ case $pkg_manager in
         ;;
 esac
 
-# Remove any existing cross-compiler installation
 rm -rf $HOME/src
 rm -rf $cc_prefix
 
-mkdir -p $HOME/src
-cd $HOME/src
+mkdir -p $RAMDISK_PATH$HOME/src
+cd $RAMDISK_PATH$HOME/src
 
 wget https://ftp.gnu.org/gnu/binutils/binutils-2.34.tar.xz
 tar -xf binutils-2.34.tar.xz
@@ -57,26 +96,26 @@ tar -xf gcc-9.4.0.tar.gz
 
 mkdir build-binutils
 cd build-binutils
-../binutils-2.34/configure --target=$cc_target --prefix="$cc_prefix" --with-sysroot --disable-nls --disable-werror
+../binutils-2.34/configure --target=$cc_target --prefix="$RAMDISK_PATH/$cc_prefix" --with-sysroot --disable-nls --disable-werror
 make -j$ncpu
 make install -j$ncpu
 
-cd $HOME/src
+cd $RAMDISK_PATH$HOME/src
 
 which -- $cc_target-as || echo "$cc_target-as is not in the PATH"
 
 mkdir build-gcc
 cd build-gcc
-../gcc-9.4.0/configure --target=$cc_target --prefix="$cc_prefix" --disable-nls --enable-languages=c --without-headers
+../gcc-9.4.0/configure --target=$cc_target --prefix="$RAMDISK_PATH/$cc_prefix" --disable-nls --enable-languages=c --without-headers
 make all-gcc -j$ncpu
 make all-target-libgcc -j$ncpu
 make install-gcc -j$ncpu
 make install-target-libgcc -j$ncpu
 
-# Set up environment variables for Bash shell
-echo "export PATH=$HOME/opt/cross/bin:\$PATH" >> ~/.bashrc
-echo "export cc_prefix=$HOME/opt/cross" >> ~/.bashrc
-echo "export cc_target=i686-elf" >> ~/.bashrc
+# move ramdisk to final if ramdisk
+if [ -n "$RAMDISK_PATH" ]; then
+    mv $RAMDISK_PATH$cc_prefix $cc_prefix
+fi
 
 # Set up environment variables for Fish shell (if Fish is installed)
 if command -v fish &>/dev/null; then
@@ -91,7 +130,7 @@ if command -v nasm &>/dev/null; then
 else
     echo "nasm not installed. Installing..."
 
-    cd $HOME/src
+    cd $RAMDISK_PATH$HOME/src
     wget https://www.nasm.us/pub/nasm/releasebuilds/2.16.01/nasm-2.16.01.tar.gz
     tar xf nasm-2.16.01.tar.gz
     cd nasm-2.16.01
@@ -105,7 +144,7 @@ if command -v grub-mkrescue &>/dev/null; then
 else
     echo "grub-mkrescue not installed. Installing..."
 
-    cd $HOME/src
+    cd $RAMDISK_PATH$HOME/src
     wget ftp://ftp.gnu.org/gnu/grub/grub-2.06.tar.gz
     tar xf grub-2.06.tar.gz
     cd grub-2.06
@@ -119,7 +158,7 @@ if command -v xorriso &>/dev/null; then
 else
     echo "xorriso not installed. Installing..."
 
-    cd $HOME/src
+    cd $RAMDISK_PATH$HOME/src
     wget https://www.gnu.org/software/xorriso/xorriso-1.5.6.tar.gz
     tar xf xorriso-1.5.6.tar.gz
     cd xorriso-1.5.6
@@ -128,3 +167,10 @@ else
 fi
 
 echo "Cross-compiler installation complete. Please restart your shell or run 'source ~/.bashrc' (or equivalent for your shell) to update your PATH."
+
+cd $HOME
+
+# Unmount the RAM disk when done
+if [ -n "$RAMDISK_PATH" ]; then
+    sudo umount /tmp/ramdisk
+fi
